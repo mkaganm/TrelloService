a944fa37e2311b8ea165eb82d57ca72aea1d4070
kagan.meric <mustafakaganmeric@gmail.com>
2023-05-06 20:56:11 +0300

    command java added

---
+++ src/main/java/core/utility/Command.java
@@ -0,0 +1,14 @@
+package core.utility;
+
+public class Command {
+
+    private Command(){}
+
+    public static void runCommand(String command) {
+        try {
+            Runtime.getRuntime().exec(command);
+        } catch (Exception e) {
+            Logging.fail("command exception.", e);
+        }
+    }
+}
